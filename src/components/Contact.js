385a0b38101ece093d33eb17bdac4fda7744fb17
GainorB <gainorbostwick@gmail.com>
2017-08-23 17:54:06 -0400

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,8 +7,8 @@ const Contact = () => {
                 <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8">
                     <h3><span className="highlight">Looking for a developer?</span></h3>
                     <p>Leave a brief message and we will finish the conversation through email :)</p>
-                    <p className="formInputs">
-                        <form action='https://formspree.io/hi@gainorbostwick.com' method='POST'>
+                    <form action='https://formspree.io/hi@gainorbostwick.com' method='POST'>
+                        <p className="formInputs">
                             <input type="text" name="name" placeholder="Name" required />
                             <input type="email" name="email" placeholder="Your Email" required />
                             <textarea id="tArea" name="content" placeholder="Brief project description" required/>
@@ -16,8 +16,8 @@ const Contact = () => {
                             <input type="hidden" name="_subject" value="In Need of Developer" />
                             <input type="text" name="_gotcha" className="hide" /><br/>
                             <input type="submit" value="Send" /><input type="reset" value="Reset" />
-                        </form>
-                    </p>
+                        </p>
+                    </form>
                 </div>
             </div>
             <hr className="hr"/>
